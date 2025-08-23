b6e9ceadf928faa0e2998e7702c139faa40fefd5
starlordv125 <starlordv125@protonmail.com>
2025-08-23 02:16:05 -0400

    Added round start screen

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
 # Technocalypse
 If you are from the VWCC computer science club, you're in the right place!
 ## File Locations
-The main executable file is located under `Technocalypse/usr/bin/game.py`.
+The main executable file is located under `Technocalypse/usr/bin/technocalypse.py`.
 All assets such as pictures, fonts, and sounds are in `Technocalypse/usr/lib/technocalypse/assets/`
 ## Debian Packaging
 As of right now the plan is to package this game into a .deb file so it can be installed onto a raspberry pi. The `DEBIAN` folder and `share` folder are only for packaging purposes, and have no impact on the actual program. Keep in mind that this is being developed for a linux system, the file paths in the program need to be formatted correctly.
 ### Example of correct format:
 ```
 usr/lib/technocalypse/assets/Techno.ttf
-usr/bin/game.py
+usr/bin/technocalypse.py
 ```
 ### Example of incorrect format:
 ```
 usr\lib\technocalypse\assets\Techno.ttf
-usr\bin\game.py
+usr\bin\technocalypse.py
 ```
 This also means certain files can only go in certain places. **All assets** need to go in `usr/lib/technocalypse/assets/`. The program file in `usr/bin/` needs to be the only file in that directory. Putting anything else in there will mess up the packaging process.
 ## License
--- usr/bin/technocalypse.py
+++ usr/bin/technocalypse.py
@@ -2,14 +2,18 @@ import pygame
 import math
 from PIL import Image, ImageSequence
 import os
+import time
 
 # Initialize pygame
 pygame.init()
 pygame.mixer.init()
+clock = pygame.time.Clock()
 
 # Sound init will go here
 menuTheme = pygame.mixer.Sound('usr/lib/technocalypse/assets/menu.mp3')
+menuTheme.set_volume(0.45)
 battleTheme = pygame.mixer.Sound('usr/lib/technocalypse/assets/battle.mp3')
+roundStartTheme = pygame.mixer.Sound('usr/lib/technocalypse/assets/roundStart.mp3')
 
 # Ends Program
 def quit_program():
@@ -28,12 +32,15 @@ screen = pygame.display.set_mode((x, y), pygame.FULLSCREEN)
 # Font init
 titleFont = pygame.font.Font('usr/lib/technocalypse/assets/Techno.ttf', (y//5))
 mediumFont = pygame.font.Font('usr/lib/technocalypse/assets/Techno.ttf', (y//10))
+roundFont = pygame.font.Font('usr/lib/technocalypse/assets/Technopath.otf', (y//10))
 
 # Picture init
 background = pygame.image.load('usr/lib/technocalypse/assets/menu.jpg')
 background = pygame.transform.scale(background, (x, y))
 
 # Game state
+count = 0
+score = 0
 health = 3 # May change later
 main_menu = True
 
@@ -52,6 +59,12 @@ def draw_menu_text():
     mediumText = mediumFont.render('Press Space to Start', True, (0, 0, 0,))
     screen.blit(mediumText, mediumText.get_rect(center=(x//2, y//2 + 200)))
 
+def draw_round_text(roundCount):
+    stroundCount = str(roundCount)
+    for dx, dy in [(2, 2), (-2, -2), (2, -2), (-2, 2)]:
+        outline_text_2 = roundFont.render(('Round ' + stroundCount), True, (255, 255, 255))
+        screen.blit(outline_text_2, outline_text_2.get_rect(center=(x//2 + dx, y//2 + dy)))
+
 # Main menu
 while main_menu == True:
     draw_background()
@@ -62,4 +75,20 @@ while main_menu == True:
         if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             pygame.quit()
             exit()
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            menuTheme.stop()
+            main_menu = False
+
+# Round start
+def round_start():
+    roundStartTheme.play()
+    roundCount = count + 1
+    start_time = time.time()
+    while (time.time() - start_time) < 4.5:
+        screen.fill((0, 0, 0))
+        draw_round_text(roundCount)
+        pygame.display.update()
+    
+# Main loop
+round_start()
 clock.tick(60)
